41101274865fd8830665d90baee53e8e69ee2192
Prabina <prabinapani.16@gmail.com>
2018-10-17 18:47:27 +0530

    Testing Jenkins 4th time

--- src/main/java/com/kharidlo/service/demo/Demo.java
+++ src/main/java/com/kharidlo/service/demo/Demo.java
@@ -2,7 +2,7 @@ package com.kharidlo.service.demo;
 
 public class Demo {
     public boolean testMethod(){
-        System.out.print("Hello ThoughtWorkers");
+        System.out.print("Hello ThoughtWorkers!!");
         return true;
     }
 }
