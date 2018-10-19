032b8676678695295b608c4062780151e4014d92
madhukar.bhandari <madhukar.bhandari@thoughtworks.com>
2018-10-19 10:13:40 +0530

    User Registration Api

--- src/main/java/com/kharidlo/service/UserRegistration/ApiController/UserRegistrationController.java
+++ src/main/java/com/kharidlo/service/UserRegistration/ApiController/UserRegistrationController.java
@@ -4,6 +4,7 @@ import com.kharidlo.service.UserRegistration.IUserRegistrationService;
 import com.kharidlo.service.UserRegistration.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +15,8 @@ public class UserRegistrationController {
     @Autowired
     IUserRegistrationService userRegistrationService;
 
-    @GetMapping
-    public int get() {
-
-        User klUser = new User("Test","Test","Test","Test");
+    @PostMapping
+    public int post(User klUser) {
         return userRegistrationService.create(klUser).getId();
     }
 }
